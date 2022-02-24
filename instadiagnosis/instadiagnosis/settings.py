7a3d178a772e2be06b106be703e07606f9e17d1b
thecoderenroute <thecoderenroute@gmail.com>
2022-02-24 21:42:05 +0530

    added html, css and js files and setup static.

--- instadiagnosis/instadiagnosis/settings.py
+++ instadiagnosis/instadiagnosis/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'diagnose'
+    'diagnose',
 ]
 
 MIDDLEWARE = [
@@ -116,9 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'diagnose/templates/diagnose/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
